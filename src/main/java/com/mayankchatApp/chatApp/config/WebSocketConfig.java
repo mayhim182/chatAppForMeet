001d51955b38ed4e7b94767a25889c9a64ba3c13
mayhim182 <mayankatnitrkl@gmail.com>
2023-10-12 12:46:16 +0530

    chat app backend

---
+++ src/main/java/com/mayankchatApp/chatApp/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.mayankchatApp.chatApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+  //Configuring our websocket endpoint and message broker
+
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry){
+    registry.addEndpoint("/websocket").withSockJS();
+  }
+
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry){
+    registry.enableSimpleBroker("/topic");
+    registry.setApplicationDestinationPrefixes("/app");
+  }
+
+
+}
